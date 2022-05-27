ed9ade2b372c0f1d228bf517af917632457d7f47
Nuruzzaman Milon <contact@milon.im>
2022-05-27 09:16:50 +0200

    Update mac settings

--- support/mac_settings.sh
+++ support/mac_settings.sh
@@ -3,6 +3,10 @@
 # hide last login info item2
 touch ~/.hushlogin
 
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+
 # choosy as browser
 # disable spotlight suggestions
 # full keyboard access; all controls
