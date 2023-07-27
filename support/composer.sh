acbeb6a9ac6d1cdbb7193236df80dcb97364a84c
Nuruzzaman Milon <contact@milon.im>
2023-07-27 07:22:02 -0400

    replace homebrew php with laravel herd

--- support/composer.sh
+++ support/composer.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 brew install composer
-composer global require laravel/valet
 composer global require psy/psysh
 
 echo 'XX -- Composer done.'
--- support/valet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-valet install
-valet trust
-mkdir -p $HOME/Code
-cd $HOME/Code
-valet park
-
-echo 'XX -- Valet done.'
