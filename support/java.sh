da756e252cab498385cc5a06355a2f7bc4c2a4bc
Nuruzzaman Milon <milon@punt.com>
2024-11-08 11:20:18 -0800

    update java install script

--- support/java.sh
+++ support/java.sh
@@ -2,11 +2,11 @@
 
 echo 'Configuring java'
 
-echo 'Installing Temurin JDK 17'
-brew install --cask temurin17
-jenv add /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/
+echo 'Installing Temurin JDK 21'
+brew install --cask temurin@21
+jenv add /Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/
 
 echo 'Setting java version'
-jenv global 17.0
+jenv global 21
 
 echo 'Java done.'
