df6a63dbb9da2fb08077dc26e46f2e98866ec42e
Nuruzzaman Milon <milon@punt.com>
2024-12-17 11:24:57 -0800

    update wezterm config

--- files/wezterm.lua
+++ files/wezterm.lua
@@ -9,6 +9,7 @@ config.font = wezterm.font {
     -- harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
 }
 config.font_size = 16
+config.line_height = 1.4
 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 1
@@ -20,7 +21,14 @@ config.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
 config.integrated_title_button_style = "MacOsNative"
 config.integrated_title_button_alignment = "Left"
 config.native_macos_fullscreen_mode = true
-config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = "BlinkingBar"
+
+config.scrollback_lines = 3000
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+config.tab_bar_at_bottom = false
+
 
 return config
