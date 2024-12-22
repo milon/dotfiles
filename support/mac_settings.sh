7cd157470158b16328f2a8af78e78a6bc24561f8
Nuruzzaman Milon <contact@milon.im>
2024-12-22 10:05:18 -0800

    Update mac_settings.sh to enhance dock autohide settings

--- support/mac_settings.sh
+++ support/mac_settings.sh
@@ -6,14 +6,13 @@ touch ~/.hushlogin
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Disable annoying disk eject warning
-defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
-
 # Change screenshot type to JPG
 defaults write com.apple.screencapture type jpg
 
 # Disable autohide animation for dock 
-defaults write com.apple.dock autohide-delay -float 0; killall Dock
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.2
+killall Dock
 
 # choosy as browser
 # disable spotlight suggestions
