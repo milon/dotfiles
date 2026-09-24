b3ca894483acdf73c353f4490f8c6e0ef055f72a
milon <milon@punt.com>
2026-09-24 12:54:27 -0700

    Change compinit config

--- files/home/zsh/compinit.zsh
+++ files/home/zsh/compinit.zsh
@@ -1,4 +1,13 @@
 # compinit turns on zsh's completion system. It must run after completion
 # functions are on fpath (zsh-completions) and before plugins that wrap
 # completion widgets, such as autosuggestions and syntax highlighting.
-autoload -Uz compinit && compinit
+# Rebuild the dump once a day. -C skips that check on later shells.
+autoload -Uz compinit
+zmodload zsh/datetime
+zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
+if [[ ! -f $zcompdump ]] || (( EPOCHSECONDS - $(stat -f %m "$zcompdump") > 86400 )); then
+    compinit
+else
+    compinit -C
+fi
+unset zcompdump
